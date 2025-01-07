9f98f827afcc1a3f5704a81e542ae2f2a18f6550
Jaimik Kadu <jaimikpkadu18@gmail.com>
2025-01-07 22:45:40 +0530

    Add pages folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import MovieCard from './components/MovieCard'
 
 function App() {
 
   return (
       <>
-
+        <MovieCard movie={{title:"Jaimik",release_date:"18/09/2002"}}/>
       </>
   )
 }
