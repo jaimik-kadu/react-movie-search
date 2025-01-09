5a175f5adacd74ed414b4722f3c4f4b8737c1bd7
Jaimik Kadu <jaimikpkadu18@gmail.com>
2025-01-09 11:09:01 +0530

    Add Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,20 @@ import './App.css'
 import Home from './pages/Home'
 import { Route, Routes } from 'react-router-dom'
 import Favorites from './pages/Favorites';
+import NavBar from './components/NavBar';
 
 function App() {
 
   return (
-    <main className='main-content'>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/favorite' element={<Favorites />} />
-      </Routes>
-    </main>
+    <div>
+      <NavBar />
+      <main className='main-content'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/favorites' element={<Favorites />} />
+        </Routes>
+      </main>
+    </div>
   );
 }
 
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+function NavBar() {
+    return (
+        <nav className="navbar">
+            <div className="nav-brand">
+                <Link>Movie App</Link>
+            </div>
+            <div className="navbar-links">
+                <Link to='/' className='nav-link'>Home</Link>
+                <Link to='/favorites' className='nav-link'>Favorites</Link>
+            </div>
+        </nav>
+    )
+}
