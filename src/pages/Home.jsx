20a6caceedde0e159b355b70f2f3fba1035ac425
Jaimik Kadu <jaimikpkadu18@gmail.com>
2025-01-09 21:03:40 +0530

    Add search functionality

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,11 +7,11 @@ function Home() {
 
     const [searchQuery, setSearchQuery] = useState("");
     const [movies, setMovies] = useState([]);
-    const[error,setError] = useState(null);
-    const[load,setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        const loadPopularMovies = async ()=>{
+        const loadPopularMovies = async () => {
             try {
                 const popularMovies = await getPopularMovies()
                 setMovies(popularMovies)
@@ -19,7 +19,7 @@ function Home() {
                 console.log(err);
                 setError("Failed to load movies...")
             }
-            finally{
+            finally {
                 setLoading(false);
             }
         }
@@ -27,9 +27,23 @@ function Home() {
         loadPopularMovies()
     }, [])
 
-    const handleSearch = (e) => {
+    const handleSearch = async (e) => {
         e.preventDefault()
-        alert(searchQuery)
+        if (!searchQuery.trim()) return
+        if (loading) return
+        setLoading(true)
+
+        try {
+            const searchResults = await searchMovies(searchQuery)
+            setMovies(searchResults)
+            setError(null)
+        } catch (err) {
+            console.log(err)
+            setError("Failed to search movies...")
+        }
+        finally {
+            setLoading(false)
+        }
         setSearchQuery("")
     }
 
@@ -39,9 +53,14 @@ function Home() {
                 <input type="text" placeholder="Search for movies..." className="search-input" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} />
                 <button type="submit" className="search-button">Search</button>
             </form>
-            <div className="movies-grid">
+
+            {error && <div className="error-message">{error}</div>}
+
+
+            {loading ? (<div className="loading">Loading...</div>) : (<div className="movies-grid">
                 {movies.map((movie) => (<MovieCard movie={movie} key={movie.id} />))}
-            </div>
+            </div>)}
+
         </div>
     )
 }
